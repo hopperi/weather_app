df3586802624d7d264c0bc12d0f17833fb9b73a3
kostolomov <kostolomov@root.local>
2025-06-24 17:13:38 +0300

    +added http server

--- main.py
+++ main.py
@@ -1,45 +1,40 @@
-import psycopg2
+from fastapi import FastAPI, HTTPException
 from geocode.geocode import get_coords
 from weather.weather_api import get_weather
 from utils.time_utils import get_local_time
-from db import connect_db, create_table, insert_weather_data, fetch_weather_data, close_connection
+from db import connect_db, insert_weather_data, close_connection
 
+app = FastAPI()
 
 connect_db()
 
-def main():
-    city = input("Введите город: ")
+@app.get("/getWeather/{city}")
+def api_get_weather(city: str):
     lat, lon = get_coords(city)
     if lat is None or lon is None:
-        print("Город не найден")
-        return
+        raise HTTPException(status_code=404, detail="Город не найден")
 
     try:
         data = get_weather(lat, lon)
         local_time = get_local_time(data["timezone"])
-        celsius = data["main"]["temp"] - 273.15
+        celsius = round(data["main"]["temp"] - 273.15, 2)
 
-        info = {
-            "Город": data["name"],
-            "Время": local_time,
-            "Температура": round(celsius, 2),
-            "Код состояния": data["cod"]
-        }
-        print(info)
         insert_weather_data(
-    city=data["name"],
-    temperature=round(celsius, 2),
-    humidity=data["main"]["humidity"],
-    weather_description=data["weather"][0]["description"]
-)
+            city=data["name"],
+            temperature=celsius,
+            humidity=data["main"]["humidity"],
+            weather_description=data["weather"][0]["description"]
+        )
 
+        return {
+            "city": data["name"],
+            "time": local_time,
+            "temperature_celsius": celsius,
+            "description": data["weather"][0]["description"]
+        }
     except Exception as e:
-        print("Произошла ошибка:", e)
+        raise HTTPException(status_code=500, detail=str(e))
 
-data = fetch_weather_data()
-for row in data:
-    print(row)
-# close_connection()
-if __name__ == "__main__":
-    main()
-close_connection()
+@app.on_event("shutdown")
+def shutdown():
+    close_connection()
